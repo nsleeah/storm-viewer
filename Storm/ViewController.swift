17306f823750f5e3c3592337b5a9a3206ad9c978
Leahnicoles <lnsimonn@hotmail.com>
2023-07-21 15:48:20 +0100

    first commit

---
+++ Storm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storm
+//
+//  Created by Leah Simon on 21/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
